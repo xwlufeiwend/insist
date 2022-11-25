491002b82ec76204a5f2ed2ffaa742a32bef01dd
xw <xwlufeiwend@163.com>
2022-11-25 14:56:11 +0800

    221125 创建对应pipeline下载文件

--- src/main/java/com/crall/insist/InsistApplication.java
+++ src/main/java/com/crall/insist/InsistApplication.java
@@ -1,12 +1,17 @@
 package com.crall.insist;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InsistApplication {
-
     public static void main(String[] args) {
+        /*Logger logger = (Logger) LoggerFactory.getLogger("org.apache.http");
+        logger.setLevel(Level.INFO);
+        logger.setAdditive(false);*/
         SpringApplication.run(InsistApplication.class, args);
     }
 
